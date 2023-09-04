62ea4e3d3a5dafbb9bdda4d3ca02783b63c1ea4e
Bahaa Aamer <bhaa.amer1.ba@gmail.com>
2023-09-04 11:19:47 +0300

    final result

--- src/app/reative-form/reative-form.component.ts
+++ src/app/reative-form/reative-form.component.ts
@@ -16,53 +16,41 @@ import {
   styleUrls: ['./reative-form.component.css'],
 })
 export class ReativeFormComponent implements OnInit {
-  // form: FormGroup;
+  form: FormGroup;
   passwordConfirm: any;
-  form: FormGroup = new FormGroup({
-    firstName: new FormControl(null, [Validators.required, this.noSpace]),
-    lastName: new FormControl(null, [Validators.required, this.noSpace]),
-    email: new FormControl(null, [Validators.required, Validators.email]),
-    password: new FormControl(null, [
-      Validators.required,
-      Validators.minLength(8),
-    ]),
-    passwordConfirm: new FormControl(null, [
-      Validators.required,
-      this.checkTest,
-    ]),
-  });
-  constructor() {}
-  checkTest(control: FormControl) {
-    console.log('controls', control.value);
-    if (this.passwordConfirm !== undefined) {
-      console.log('passwordConfirm', this.passwordConfirm);
-    }
 
-    if (control?.value !== null && this.passwordConfirm) {
-      console.log('test');
-    }
-    return null;
-  }
+  constructor() {}
+
   ngOnInit(): void {
-    // this.form = new FormGroup({
-    //   firstName: new FormControl(null, [Validators.required, this.noSpace]),
-    //   lastName: new FormControl(null, [Validators.required, this.noSpace]),
-    //   email: new FormControl(null, [Validators.required, Validators.email]),
-    //   password: new FormControl(null, [
-    //     Validators.required,
-    //     Validators.minLength(8),
-    //   ]),
-    //   passwordConfirm: new FormControl(null, [Validators.required, checkPass]),
-    // });
+    this.form = new FormGroup({
+      firstName: new FormControl(null, [Validators.required, this.noSpace]),
+      lastName: new FormControl(null, [Validators.required, this.noSpace]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      passwordConfirm: new FormControl(null, [
+        Validators.required,
+        this.checkPass.bind(this),
+      ]),
+    });
   }
-  submit() {
-    console.log(this.form);
-  }
+  submit() {}
 
   noSpace(control: FormControl) {
     if (control.value != null && control.value.indexOf(' ') != -1) {
       return { noSpace: true };
     }
     return null;
+  }
+
+  checkPass(control: FormControl) {
+    const password = control.root.get('password');
+    if (password !== null && control.value !== password.value) {
+      return { passwordMissmatch: true };
+    } else {
+      return null;
+    }
   }
 }
